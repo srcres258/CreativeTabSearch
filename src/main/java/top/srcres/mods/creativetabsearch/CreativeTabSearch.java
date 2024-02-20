301f5234321b85ce89d59a124ec56ac41bde2e51
src_resources <src.res.211@gmail.com>
2024-02-20 12:33:14 +0800

    Migrate to my own mod settings
    Signed-off-by: src_resources <src.res.211@gmail.com>

---
+++ src/main/java/top/srcres/mods/creativetabsearch/CreativeTabSearch.java
@@ -0,0 +1,8 @@
+package top.srcres.mods.creativetabsearch;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(CreativeTabSearch.MODID)
+public class CreativeTabSearch {
+    public static final String MODID = "creativetabsearch";
+}
