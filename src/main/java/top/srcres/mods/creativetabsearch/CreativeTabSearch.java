e91ea77c56c4e0d6c5f11a1c453d8b69651b9616
src_resources <src.res.211@gmail.com>
2024-02-22 03:05:04 +0800

    Clean up unused import sentences
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- src/main/java/top/srcres/mods/creativetabsearch/CreativeTabSearch.java
+++ src/main/java/top/srcres/mods/creativetabsearch/CreativeTabSearch.java
@@ -1,17 +1,10 @@
 package top.srcres.mods.creativetabsearch;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Items;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
 @Mod(CreativeTabSearch.MODID)
