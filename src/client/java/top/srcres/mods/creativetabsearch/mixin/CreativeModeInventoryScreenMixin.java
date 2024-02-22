c2517b0fb086e8004ae4bd72b8cde8362f17c2ec
src_resources <src.res.211@gmail.com>
2024-02-22 18:50:27 +0800

    Migrate from NeoForge 1.20.4 to Fabric 1.20.4
    Signed-off-by: src_resources <src.res.211@gmail.com>

---
+++ src/client/java/top/srcres/mods/creativetabsearch/mixin/CreativeModeInventoryScreenMixin.java
@@ -0,0 +1,155 @@
+package top.srcres.mods.creativetabsearch.mixin;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
+import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.CreativeModeTab;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(CreativeModeInventoryScreen.class)
+public abstract class CreativeModeInventoryScreenMixin extends EffectRenderingInventoryScreen<CreativeModeInventoryScreen.ItemPickerMenu> {
+//    @Unique
+//    protected EditBox tabSearch_editBox;
+//    @Unique
+//    protected Button tabSearch_clearButton;
+//    @Shadow
+//    @Final
+//    private List<CreativeTabsScreenPage> pages;
+//    @Shadow
+//    private CreativeTabsScreenPage currentPage;
+//
+    public CreativeModeInventoryScreenMixin(CreativeModeInventoryScreen.ItemPickerMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
+        super(pMenu, pPlayerInventory, pTitle);
+    }
+//
+//    @Shadow
+//    private void selectTab(CreativeModeTab pTab) {
+//    }
+//
+//    @Inject(method = "init", at = @At("RETURN"))
+//    protected void initTabSearch(CallbackInfo ci) {
+//        // Only enable the creative tab searching ability when the specific game mode is satisfied
+//        // as mentioned in vanilla code.
+//        if (this.minecraft.gameMode.hasInfiniteItems()) {
+//            this.tabSearch_editBox = new EditBox(this.font, this.leftPos, this.topPos - 65,
+//                    this.imageWidth - 20, 15, this.tabSearch_editBox, Component.literal(""));
+//            this.tabSearch_editBox.setResponder(this::tabSearch_updateTabSearch);
+//            this.addWidget(this.tabSearch_editBox);
+//            this.tabSearch_clearButton = Button.builder(Component.literal("X"), this::tabSearch_clearButtonClicked)
+//                    .bounds(this.leftPos + this.imageWidth - 20, this.topPos - 65, 20, 15).build();
+//            this.tabSearch_clearButton.active = !this.tabSearch_editBox.getValue().isEmpty();
+//            this.addRenderableWidget(this.tabSearch_clearButton);
+//        }
+//    }
+//
+//    @Inject(method = "render", at = @At("RETURN"))
+//    protected void renderTabSearch(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick, CallbackInfo ci) {
+//        if (this.tabSearch_editBox != null)
+//            this.tabSearch_editBox.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+//    }
+//
+//    @Inject(method = "charTyped", at = @At("HEAD"), cancellable = true)
+//    protected void charTypedTabSearch(char pCodePoint, int pModifiers, CallbackInfoReturnable<Boolean> cir) {
+//        // The callback has to be invoked manually since the origin listener implementation has been overwritten
+//        // by CreativeModeInventoryScreen within method CreativeModeInventoryScreen#charTyped.
+//        if (this.tabSearch_editBox.charTyped(pCodePoint, pModifiers)) {
+//            cir.setReturnValue(true);
+//            cir.cancel();
+//        }
+//    }
+//
+//    @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
+//    protected void keyPressedTabSearch(int pKeyCode, int pScanCode, int pModifiers, CallbackInfoReturnable<Boolean> cir) {
+//        // Need to replace vanilla key pressing logic when editing tab search EditBox.
+//        if (this.tabSearch_editBox.isFocused()) {
+//            // Handle Esc quitting screen logic at first, otherwise the player
+//            // may feel weird for being unable to close the screen.
+//            if (pKeyCode == 256 && this.shouldCloseOnEsc()) {
+//                this.onClose();
+//                cir.setReturnValue(true);
+//            } else {
+//                cir.setReturnValue(this.tabSearch_editBox.keyPressed(pKeyCode, pScanCode, pModifiers));
+//            }
+//            cir.cancel();
+//        }
+//    }
+//
+//    @Unique
+//    private void tabSearch_updateTabSearch(String pNewText) {
+//        if (pNewText.isEmpty()) {
+//            this.tabSearch_editBox.setTextColor(EditBox.DEFAULT_TEXT_COLOR);
+//            tabSearch_resetCreativeTabPages(CreativeModeTabRegistry.getSortedCreativeModeTabs());
+//            this.tabSearch_clearButton.active = false;
+//        } else {
+//            List<CreativeModeTab> result = tabSearch_getMatchingTabs(pNewText);
+//            if (result.isEmpty()) {
+//                this.tabSearch_editBox.setTextColor(0xff0000);
+//                tabSearch_resetCreativeTabPages(CreativeModeTabRegistry.getSortedCreativeModeTabs());
+//            } else {
+//                this.tabSearch_editBox.setTextColor(EditBox.DEFAULT_TEXT_COLOR);
+//                tabSearch_resetCreativeTabPages(result);
+//            }
+//            this.tabSearch_clearButton.active = true;
+//        }
+//    }
+//
+//    @Unique
+//    private static List<CreativeModeTab> tabSearch_getMatchingTabs(String searchStr) {
+//        ArrayList<CreativeModeTab> result = new ArrayList<>();
+//        for (CreativeModeTab tab : CreativeModeTabRegistry.getSortedCreativeModeTabs()) {
+//            if (tab.getDisplayName().getString().contains(searchStr)) {
+//                result.add(tab);
+//            }
+//        }
+//        return result;
+//    }
+//
+//    @Unique
+//    private void tabSearch_resetCreativeTabPages(List<CreativeModeTab> tabList) {
+//        this.pages.clear();
+//        int tabIndex = 0;
+//        List<CreativeModeTab> currentPage = new ArrayList<>();
+//
+//        for (CreativeModeTab sortedCreativeModeTab : tabList) {
+//            currentPage.add(sortedCreativeModeTab);
+//            ++tabIndex;
+//            if (tabIndex == 10) {
+//                this.pages.add(new CreativeTabsScreenPage(currentPage));
+//                currentPage = new ArrayList();
+//                tabIndex = 0;
+//            }
+//        }
+//
+//        if (tabIndex != 0) {
+//            this.pages.add(new CreativeTabsScreenPage(currentPage));
+//        }
+//
+//        if (this.pages.isEmpty()) {
+//            this.currentPage = new CreativeTabsScreenPage(new ArrayList<>());
+//        } else {
+//            this.currentPage = this.pages.get(0);
+//        }
+//
+//        this.selectTab(tabList.get(0));
+//    }
+//
+//    @Unique
+//    private void tabSearch_clearButtonClicked(Button button) {
+//        this.tabSearch_editBox.setValue("");
+//        button.active = false;
+//    }
+}
--- src/main/java/top/srcres/mods/creativetabsearch/CreativeTabSearch.java
+++ src/main/java/top/srcres/mods/creativetabsearch/CreativeTabSearch.java
@@ -1,35 +1,9 @@
 package top.srcres.mods.creativetabsearch;
 
-import com.mojang.logging.LogUtils;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.loading.FMLEnvironment;
-import org.slf4j.Logger;
+import net.fabricmc.api.ModInitializer;
 
-@Mod(CreativeTabSearch.MODID)
-public class CreativeTabSearch {
-    public static final String MODID = "creativetabsearch";
-
-    public static CreativeTabSearch instance;
-
-    private Logger logger;
-
-    public CreativeTabSearch(IEventBus modEventBus) {
-        // This mod is only client-sided.
-        if (!FMLEnvironment.dist.equals(Dist.CLIENT))
-            throw new IllegalDistException();
-
-        instance = this;
-
-        logger = LogUtils.getLogger();
-    }
-
-    public static CreativeTabSearch getInstance() {
-        return instance;
-    }
-
-    public Logger getLogger() {
-        return logger;
+public class CreativeTabSearch implements ModInitializer {
+    @Override
+    public void onInitialize() {
     }
 }
